5a4d07370ff8f3e83c25cea2ec1eae2454812979
kostili-tec <weh4teyou@gmail.com>
2023-07-25 23:23:42 +0300

    feat: add supporting prod & dev .env's

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
 
 async function bootstrap() {
+  dotenv.config();
+
+  const PORT = process.env.PORT || 4000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+
+  await app.listen(PORT, () =>
+    console.log(`Server started on port = ${PORT} `),
+  );
 }
 bootstrap();
